fc78c7715e947400075e46fa20db319dc27de9e1
utku <utkuuyucel@gmail.com>
2019-03-15 16:33:24 +0300

    initial commit

---
+++ app/src/main/java/com/roasloa/readmyshit/Controller/PoemReaderActivity.kt
@@ -0,0 +1,29 @@
+package com.roasloa.readmyshit.Controller
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.Window
+import com.roasloa.readmyshit.Model.Poem
+import com.roasloa.readmyshit.R
+import com.roasloa.readmyshit.Utilities.EXTRA_POEM_TITLE
+import kotlinx.android.synthetic.main.activity_poem_reader.*
+
+class PoemReaderActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_poem_reader)
+
+
+
+        val poem = intent.getParcelableExtra<Poem>(EXTRA_POEM_TITLE)
+
+        poemReaderTitle.text = poem.title
+        poemReaderContent.text = poem.content
+        poemReaderContent.movementMethod = ScrollingMovementMethod()
+    }
+}
---
+++ app/src/main/java/com/roasloa/readmyshit/Services/PoemDataService.kt
@@ -0,0 +1,438 @@
+package com.roasloa.readmyshit.Services
+
+import com.roasloa.readmyshit.Model.Poem
+
+object PoemDataService {
+
+    val poemList = listOf<Poem>(Poem("Gelmeyecek", "Merdivenleri sarsan adımlarla koşsam da, her basamağı bir öncekinden daha hızlı aşsam da, nefes almak, soluklanmak için duraklamasam da gelmeyecek.\n" +
+            "\n" +
+            "Düşünmeyi bıraktığım anlarda bile aklımı dolduran tek düşünce.\n" +
+            "\n" +
+            "Suskun olduğum zamanlar, yokluğu beni hüzünlü bir müzik parçası gibi yavaş yavaş küstürüyor hayata. Kelimelerin bir anlamı yok, asla da olmadı. Yine de; bir şeyler karalamanın, kelimelerin —hayır, beynime doluşan o'nun— verdiği his için tekrar tekrar, tekrar tekrar, tekrar tekrar saçmalamayı seviyorum.\n" +
+            "\n" +
+            "Çünkü benimle. Yokluğunun verdiği o yıkıcı bütünsüzlüğe rağmen. Benimle. Biliyorum, beklemek zor, ama hiç gelmeyeceğini bilmek...\n" +
+            "\n" +
+            "Bir gece ansızın gelse? Hayal de olsa, gerçekliğin hiçbir kuralına uymasa da...\n" +
+            "\n" +
+            "Gelse?", "ilkay_yavru", 0 ),Poem("No Stalji", "Kendini bir başkası olarak gör\n" +
+            "Gök gürültülü\n" +
+            "Radyodan gelen kısık sesli müzik\n" +
+            "Değişen kararlar\n" +
+            "Ölümün mistik kokusu\n" +
+            "Sarhoş gençlerin ağızlarından dinmeyen şarkılar\n" +
+            "Türküler\n" +
+            "Korku dolu anlar\n" +
+            "Sadece çık\n" +
+            "Dalgalan\n" +
+            "Yalanlar\n" +
+            "Macera\n" +
+            "Trenin vagonları\n" +
+            "\n" +
+            "Kendin gibi ol ama fazla abartma\n" +
+            "Düşünmesi güç\n" +
+            "Ortalama\n" +
+            "Hatırlanan ama tam olarak gerçek olmayan anılar\n" +
+            "Anlamsız kelimeler bütünü\n" +
+            "Usturuplu mübalağalar\n" +
+            "Kaybolmuş satırlar\n" +
+            "Kelime oyunları\n" +
+            "Frekans kopuklukları\n" +
+            "Görülmeyen kumlar\n" +
+            "Zamanın\n" +
+            "Okyanusun\n" +
+            "İncinmiş masallar\n" +
+            "Mutlu hissettiren gülümsemeler\n" +
+            "Aşk dolu sırıtışlar\n" +
+            "Sevgi dolu kahkahalar\n" +
+            "\n","ilkay_yavru", 0), Poem("Time", "There were no lies\n" +
+            "Only the truth\n" +
+            "The truth which whispers every moment\n" +
+            "The whisper that gets stronger as time passes by\n" +
+            "The clock is ticking and there is no way to shut it down\n" +
+            "Tick tock\n" +
+            "Tick tock\n" +
+            "When the truth finally begins to reveal itself\n" +
+            "Without a doubt, it will be the end\n" +
+            "Slowly\n" +
+            "But surely\n" +
+            "The time is ticking\n" +
+            "Eventually\n" +
+            "The truth\n" +
+            "Will reveal itself\n" +
+            "...\n" +
+            "Oh it hurts so much to know\n" +
+            "It's a burden\n" +
+            "It burns\n" +
+            "Things could have been better\n" +
+            "If only one was telling the lies\n" +
+            "Tick tock\n" +
+            "Tick tock\n" +
+            "When the time comes\n" +
+            "Tick tock\n" +
+            "Tick tock\n" +
+            "There is no returning back\n" +
+            "Tick tock\n" +
+            "Tick tock\n" +
+            "\n", "ilkay_yavru", 0), Poem("The", "Keep an eye on yourself, would you?\n" +
+            "There is no need to worship the 0 prophets, just enjoy the show.\n" +
+            "Every moment has its special charm, even the worst ones.\n" +
+            "Fading images don't really lie.\n" +
+            "Some has many and some has few.\n" +
+            "Sun is shouting from your behind, \"Just let go\".\n" +
+            "Don't love every second of the day, just don't gaze into nothingness.\n" +
+            "Understand that differences makes the most similarities, hard to grasp and easy to hold on to.\n" +
+            "You are not made out of stone, you can't be easily shaped, but your ideas can.\n" +
+            "Light years has passed from your birth and nothing has really changed, all the nonsense is still present.\n" +
+            "Calm down you little, calm down.\n" +
+            "Hush now, don't you sob.\n" +
+            "What it means to be alone differentiates from one to another.\n" +
+            "Don't just lose your grip, the hardest parts are the most fun.\n" +
+            "Enjoy the show, enjoy the show.\n" +
+            "Sky is going to be pink again, if you continue to expect the unexpected.\n" +
+            "Don't imagine the worse, you are living the worst.\n" +
+            "Vulnerable souls are the deadliest to others.\n" +
+            "Enhance your view once again.\n" +
+            "Don't let go, just hold it in once more.\n" +
+            "Hush again; never answer your dark, inner questions.\n" +
+            "Some questions are not there to be answered.\n" +
+            "Wait.\n" +
+            "\n", "ilkay_yavru", 0),Poem("Av", "Porselen tabağın üzerinde yanan son mektubunun dumanı doldurur havayı yavaşça. Hafif sarhoşluğun verdiği yorgunlukla adını haykırırım içinden. Sana hiç de yakışmayan o parfümünün kokusunun yankıları hâlâ capcanlı.\n" +
+            "Gördüklerimi, hayal ettiklerimi resmedememin verdiği yorgunluk hissinin hızı başımı döndürür.\n" +
+            "...\n" +
+            "Ya şimdi?\n" +
+            "Şimdi bana hissettirdiğin tek şey, insanlara sorgulatamadığım kamufle noksanlığın. ", "ilkay_yavru", 0),Poem("H", "Sıradan olmayı beceremedim,\n" +
+            "Sinirliyim size,\n" +
+            "Sınırlayamıyorum kendimi çünkü,\n" +
+            "Daraltamıyorum dağarcığımı,\n" +
+            "Sizin gibi olamıyorum,\n" +
+            "Tekrarlamasam anlamıyorsunuz,\n" +
+            "Üzgünüm.\n" +
+            "Bağışlayın.\n" +
+            "Affedin.\n" +
+            "\n" +
+            "Öfkem birine değil de bir şeylere daha çok. Hani şu anlamlandıralamayan, anlamlandırmaya kalksam anlatamayışıma götünüzle gülüp, eleştireceğiniz türden şeyler. Olmadığını düşündüğünüz bir şeye sinirlenmek çok komik. Tavsiye ederim, bağımlılar gibi sürekli aynı şeye sinirlenmenin verdiği haz, eğer her gününüz bir önceki gün kadar kötü geçiyorsa, yaşamaya değer nadir şeylerden. Hiçbir şeyden emin olamamam, sanırım, olgunluğa adımlarımın noksanlığından ileri gelmekte.\n" +
+            "\n" +
+            "Öfkelendiğim şey kendimim sanırım. Hayatınızda size umut veren ve besleyen sadece iki şey varsa ve geriye kalan hiçbir şey sizi yeterince heyecanlandırmıyorsa hayatınızı biraz gözden geçirmeniz gerekir. Sanırım bu bitiremeyeceğim yazılardan birisi olacak. Bitiremeyecek oluşum; satır aralarına sığmayacağını düşünsem de sığdırmaya çalıştıklarım, açmak istemediğim kapıların ne olduklarından bile bahsetmeyip önlerinden geçişlerim, sahip olup olmadığımı sorgulayanlara karşı kendi iç dünyamda ortaya beliren ikilemde sürekli inkar etmeye çalıştığım intihar meyilim gibi sebeplerden kaynaklanacak. Bir de olduğum yere çakılmam var tabi.\n" +
+            "\n" +
+            "...", "ilkay_yavru", 0),Poem("Nezaketi Öldürmek", "Şu andan itibaren\n" +
+            "Geriye bir şey kalmadı\n" +
+            "Öfke ve kin\n" +
+            "Şimdi biraz uzakta\n" +
+            "\n" +
+            "Gölgenle beraber geldiğinde\n" +
+            "Bekleyişim sona ermişti\n" +
+            "Şimdi sakinleş\n" +
+            "Derin bir nefes al\n" +
+            "Aydınlığı hak et\n" +
+            "Okyanus, olabildiğince mavi\n" +
+            "\n" +
+            "Mavinin tonlarını vurgula\n" +
+            "Ve devam et, sayıkla\n" +
+            "Sefalet evinden geriye\n" +
+            "Külleri dışında bir şey kalmadı\n" +
+            "\n" +
+            "Uzakta\n" +
+            "Sessizlik ve dünyanın sonu\n" +
+            "Ya da sadece bir muamma\n" +
+            "Kollarını aç\n" +
+            "Yukarı bak ve gör\n" +
+            "Başarısızlık merdivenleri\n" +
+            "Tırmanması zor\n" +
+            "\n" +
+            "Çiğ tekliflerin aldatışının\n" +
+            "Fırtınası kapıda\n" +
+            "Kolaylıkla tutulur\n" +
+            "Başka bir şey kalmamış\n" +
+            "Tek istisna\n" +
+            "Alakasızlığın mahkemesi\n" +
+            "İyileşme izi yok\n" +
+            "\n" +
+            "Gerçekten olduğun şey\n" +
+            "Batan bir kıta şimdi\n" +
+            "Sen yine de\n" +
+            "Anlatmaya devam et\n" +
+            "Yenidoğanların ruhlarına\n" +
+            "Günışığının ne kadar az ulaştığını\n" +
+            "\n" +
+            "Gizli bir mutsuzluğa\n" +
+            "Kirli bir yakınlaşma\n" +
+            "Kelimeler kifayetsiz\n" +
+            "\n" +
+            "Derin, kazanılması zor takdir\n" +
+            "Anarşi şarkıları ise\n" +
+            "Ormanda kayıp\n" +
+            "Soğukta yanıyor\n" +
+            "Aynı zamanda\n" +
+            "Başkalarının rüyaları kadar ıslak\n" +
+            "Beyaz\n" +
+            "\n" +
+            "Zincirlerle savaş\n" +
+            "Kıyı hâlâ kalabalık\n" +
+            "Kuralları bilinmesen de\n" +
+            "Ayakta kalmaya devam et\n" +
+            "\n" +
+            "Perdelerin ardında\n" +
+            "Daima yeni yerler var\n" +
+            "Güvenli bölgeler\n" +
+            "\n" +
+            "Sen ve ben\n" +
+            "Gözler için çok yoğun\n" +
+            "Katilin notları gibi\n" +
+            "Parçaları biraraya getir\n" +
+            "Bitir ve yorul\n" +
+            "Sol solabildiğince\n" +
+            "Gökyüzü hâlâ derin uyku kırmızısı\n" +
+            "\n" +
+            "Çalınmış konuşmalardan çık\n" +
+            "İnan\n" +
+            "Ufak sırlar görünür kılar kelimeleri\n" +
+            "\n" +
+            "Anlamını yitirmiş çığlıkların\n" +
+            "Ölümcül ama dengeli\n" +
+            "Bir o kadar da zavallı kavrayışı\n" +
+            "Bilinmeyenin izleri\n" +
+            "İşkencenin hızından ibaret\n" +
+            "Saklamak için beni bağla\n" +
+            "\n" +
+            "Şarkı sözleri olmadan\n" +
+            "Gece çok genç ve\n" +
+            "Suçlulardan yorgun\n" +
+            "Sen sen ol\n" +
+            "Ağıt yakmış sabahvari\n" +
+            "Kalbini esnet\n" +
+            "\n" +
+            "Bambaştan başla\n" +
+            "Aptal hüzünlerin mezarında\n" +
+            "Günler buzlanırken\n" +
+            "Sarhoşluğunu yarıya indir\n" +
+            "Ki sallansın anneler\n" +
+            "\n" +
+            "Tutulmanın altına uzan\n" +
+            "Eve geridönüşsüzlük can yakıyor\n" +
+            "İblisin güneşi ve\n" +
+            "Kurşunların kontrolü altında\n" +
+            "Isın\n" +
+            "\n" +
+            "Dene\n" +
+            "Reddet\n" +
+            "Parçalara ayrıl\n" +
+            "Umudun urganına başını yasla\n" +
+            "\n" +
+            "Kontrolsüzlükte uzmanlaşmış\n" +
+            "İhtirasın ekşi kreması\n" +
+            "Geçip gidiyor şimdi\n" +
+            "Günbegün çığlık at\n" +
+            "Kork ve\n" +
+            "Yavaşla\n" +
+            "\n" +
+            "Pişmanlıklarla dolu birikintinin\n" +
+            "Etrafında dolaş\n" +
+            "Korkma\n" +
+            "Hâlâ zaman var\n" +
+            "Boşluğu hızlandır\n" +
+            "Tekliğe ulaş\n" +
+            "Ki mutlak belalar\n" +
+            "İyi bir huzursuzluk getirsin\n" +
+            "\n" +
+            "Derin ve ipe asılmış\n" +
+            "Minik bir anda\n" +
+            "Çoktan dökülmüş gözyaşlarını paylaş\n" +
+            "Dur\n" +
+            "Evrensel bencillik yasalarını farket\n" +
+            "\n" +
+            "Uzun bir yolda\n" +
+            "Tereddütle uyan\n" +
+            "Kastedilmeyen kasıtlar ve\n" +
+            "Korunmasız sorular bile\n" +
+            "Aldırışsız kalsın\n" +
+            "\n" +
+            "Uzaktaki bulutluluğun mahremiyeti\n" +
+            "Duraklatılmış melodiler gibi ısırırken\n" +
+            "Fikirlerini uzaklaştır\n" +
+            "Yaşanmaya değer krizlerin içinden çık\n" +
+            "\n" +
+            "Ortada buluşabiliriz\n" +
+            "Hakedişin kanlarında\n" +
+            "Sakince nefes nefese kal\n" +
+            "\n" +
+            "Mesafeyi koru\n" +
+            "Yanan hislerini tut\n" +
+            "Yalan söyle ve uzan\n" +
+            "Şşş\n" +
+            "Sayacak daha az sayı var artık\n" +
+            "Doğal uyanışın gizli bahçesinde\n" +
+            "Büyüyü boz\n" +
+            "\n" +
+            "Pozisyon alamamış\n" +
+            "Uykulu omuzlar\n" +
+            "Bulunmak için bekleyen\n" +
+            "Tamamlanmamış büyük amaçlar\n" +
+            "Sorulara dolanmış\n" +
+            "Zehirlenmiş kutsal görev\n" +
+            "Gündoğumunda tuzaklara yakalanmış\n" +
+            "Harcanmış nefesler\n" +
+            "Asla bitmeyen susuzluk\n" +
+            "...\n" +
+            "Boyaları gizle\n" +
+            "Korkmana gerek yok\n" +
+            "Sallantılar artık geçti\n" +
+            "\n" +
+            "Sadece bir günlüğüne\n" +
+            "İçgüdüler tekrar gelecek\n" +
+            "Şu simsiyah ninniler\n" +
+            "Acayip tutkuların enkazında\n" +
+            "Defoluşun yaşamboyuluğu tadındakiler\n" +
+            "\n" +
+            "Herzamansızlığın bitmeyen döngüsünde\n" +
+            "Korkularını da değiştirmenin vakti geldi\n" +
+            "Amaçsız hayatta kalış mücadelenin\n" +
+            "Sürekli dile getirdiğin dilemmasını\n" +
+            "\n" +
+            "Sakin duraksamalarla\n" +
+            "Rahatla\n" +
+            "Yinele kendini\n" +
+            "Yenile kinini\n" +
+            "Baştan başla başlayamadığında\n" +
+            "Unutmayı unutma\n" +
+            "\n" +
+            "Aydoğumunun tacizi\n" +
+            "Reddediş tohumlarına uzanıyor\n" +
+            "Hataları yutkunmak için\n" +
+            "Baygınlığını toparla\n" +
+            "Ateş et\n" +
+            "...\n" +
+            "Kalpte bir mermi\n" +
+            "\n" +
+            "Gökdelenler\n" +
+            "Şu, hiçliğin boşluğunu dolduranlar\n" +
+            "Yırtılmış fotoğraflar gibi yıkılıp gittiler şimdi\n" +
+            "\n" +
+            "Kutsalsızlığın sınırı yoksa bile\n" +
+            "Sayılamazlığı listele\n" +
+            "Komforsuzluğun sonsuz ihtimalini\n" +
+            "\n" +
+            "Korunmuş sürrealizmi bul\n" +
+            "Satılmış yaşlı soğuk kadar kalın olanı\n" +
+            "Değişebilinirliği tahmin et\n" +
+            "\n" +
+            "Salgınların hemşiresinin\n" +
+            "Çoksuzluk önerisine\n" +
+            "Aldanma\n" +
+            "Ve gel\n" +
+            "Derimin üstüne yerleş\n" +
+            "Beş keş eşimle birlikte\n" +
+            "\n" +
+            "Sıradan saçmalıkları\n" +
+            "Keşke reddedebilsen\n" +
+            "Bir daha susuyorum\n" +
+            "Ben sadece\n" +
+            "Kendi sanatsalsızlığında\n" +
+            "Şiirlerini boyayan\n" +
+            "Bir enstrümanım\n" +
+            "\n" +
+            "Korkmaktan da korkma\n" +
+            "Hâlâ hatırlıyor musun\n" +
+            "Çamaşır iplerine astığın\n" +
+            "Afacan masallarını\n" +
+            "\n" +
+            "Daha kısa uykularda\n" +
+            "Ölüm aşkıyla ıslık çal\n" +
+            "Evrenler kadar küçük detayların\n" +
+            "Daracık güneylerinden atla\n" +
+            "Hayatta kalıştan vazgeç\n" +
+            "Çünkü\n" +
+            "Biliyorum ki\n" +
+            "İğnelerinde yıldızlar gizli\n" +
+            "\n" +
+            "Cennet bahçesine giden tren yolu\n" +
+            "Her zamankinden daha kalabalık\n" +
+            "Ama bizim biletlerimiz\n" +
+            "Yıllar önce alınmış\n" +
+            "Çünkü biliyorsun ki\n" +
+            "Içten içe çatlağız biz de\n" +
+            "\n" +
+            "Saçılmış kanların seferlerine\n" +
+            "Ve sahte yeniden doğuşlara yalvar\n" +
+            "İnançsızlığın tatmin etmeyişi üzerine\n" +
+            "Gülüşünü yemin ettir\n" +
+            "\n" +
+            "Memnuniyet dalgaları esiyor\n" +
+            "Ama birdenbirelik\n" +
+            "Geçici\n" +
+            "Gidici\n" +
+            "Yıkıcı\n" +
+            "\n" +
+            "Birileri kaldırımları yaralıyor\n" +
+            "Sen ise\n" +
+            "Arafta sıkışmışcasına\n" +
+            "Hayretler ediyorsun\n" +
+            "Harekete geç\n" +
+            "\n" +
+            "Ayışığından bozma duman halkasının\n" +
+            "Söylenmemiş sessizliğin aldatıcılığından\n" +
+            "Kork\n" +
+            "Elde edilemeyen yenilgiye tırman\n" +
+            "Ve işte\n" +
+            "Orada\n" +
+            "Ufak bir esinti\n" +
+            "Rüzgarı da kat kendine\n" +
+            "\n" +
+            "Tozlanmış paslar\n" +
+            "İnanacak onca sadeceler\n" +
+            "İnançsızların toplanabilir duaları\n" +
+            "Nefes alınan yerlerin hayali\n" +
+            "Yola çıkmış kabuslar\n" +
+            "Öpücüklerin sarımtırak çığlıkları\n" +
+            "...\n" +
+            "Dur\n" +
+            "\n" +
+            "Oluşamayan dakikaların rastgeleliğine\n" +
+            "Hızlı bir geçiş yap\n" +
+            "Mucizelere inanır gibi\n" +
+            "Terk et güneylerini\n" +
+            "\n" +
+            "Küçük sütunların yarına uzanışının\n" +
+            "Doğal olmayan doğum izlerini\n" +
+            "Hisset\n" +
+            "Sabret\n" +
+            "Ve\n" +
+            "Yok ol\n" +
+            "\n" +
+            "Venüsteki meydanlarda\n" +
+            "Anında geçen onyılların uzaysal tembelliği\n" +
+            "Boyutsuzlaştırsın iç dünyanı\n" +
+            "Bil ki\n" +
+            "Kelimeler hâlâ kifayetsiz\n" +
+            "\n" +
+            "Değiştokuş edilemez korkaklıklar\n" +
+            "Açgözlü akşamüstleri kadar yalnız\n" +
+            "Başıboşluğun hazzını\n" +
+            "Tatlıca tahmin et\n" +
+            "Mutlak kayboluşun yollarında\n" +
+            "Daha çılgın elvedalara kapıl\n" +
+            "\n" +
+            "İntihar notlarının kafa karıştırıcılığında\n" +
+            "Boğul şimdi\n" +
+            "Yoldan geçip gidenlerin çaylaklığı\n" +
+            "Ve\n" +
+            "Arkaplanların figür noksanlığı kadar\n" +
+            "Küçük sonsuzluklarda\n" +
+            "\n" +
+            "Yıkıcı iyileşmeler\n" +
+            "Aşk dolu ufak mutlu çelişkiler\n" +
+            "Kalkışların köleleri\n" +
+            "Zalim olmayan hayal kırıklıkları\n" +
+            "...\n" +
+            "Topla ve hepsini bağla\n" +
+            "Onlar sadece\n" +
+            "Yanlış renk seçimleri\n" +
+            "\n" +
+            "Zorla kazanılmış düz ve mat vadilerde\n" +
+            "Yusufçukların arasında süzül şimdi\n" +
+            "İç çek\n" +
+            "Ve herkese veda ettiğimi ilet\n" +
+            "Derin bir uykuyu hakettin işte\n" +
+            "Tatlı rüyalar\n" +
+            "\n" +
+            "\n", "ilkay_yavru", 0))
+}
