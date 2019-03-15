175f9b79fa7f4c998c931bd28fe82135e85733c6
utku <utkuuyucel@gmail.com>
2019-03-15 23:01:16 +0300

    added a popup window, but I would say it's the UGLIEST

--- app/src/main/java/com/roasloa/readmyshit/Controller/MainActivity.kt
+++ app/src/main/java/com/roasloa/readmyshit/Controller/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import com.roasloa.readmyshit.Adapters.PoemCoverAdapter
 import com.roasloa.readmyshit.R
 import com.roasloa.readmyshit.Services.PoemDataService
@@ -13,16 +17,35 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var adapter: PoemCoverAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         adapter = PoemCoverAdapter(this, PoemDataService.poemList){poem ->
 
             val poemSelectionIntent = Intent(this, PoemReaderActivity::class.java)
             poemSelectionIntent.putExtra(EXTRA_POEM_TITLE, poem)
-            startActivity(poemSelectionIntent)
+//            startActivity(poemSelectionIntent)
+
+            val inflater: LayoutInflater = LayoutInflater.from(this)
+
+
+            val popupView = inflater.inflate(R.layout.popup_window, null)
+
+            val width = LinearLayout.LayoutParams.WRAP_CONTENT
+            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+            var focusable = true
+
+            val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+            popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0)
+
+
 
         }
+
+
 
         val layoutManager = GridLayoutManager(this, 2)
 
