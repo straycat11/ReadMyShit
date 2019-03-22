019a759a41afe3ca1a9e190f435cb6481ff8e4d8
utku <utkuuyucel@gmail.com>
2019-03-22 13:46:05 +0300

    Made some tweaks on popupwindow

--- app/src/main/java/com/roasloa/readmyshit/Controller/MainActivity.kt
+++ app/src/main/java/com/roasloa/readmyshit/Controller/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
+import android.util.Log
+import android.util.TypedValue
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.PopupWindow
+import android.widget.TextView
 import com.roasloa.readmyshit.Adapters.PoemCoverAdapter
 import com.roasloa.readmyshit.R
+import com.roasloa.readmyshit.R.layout.*
 import com.roasloa.readmyshit.Services.PoemDataService
 import com.roasloa.readmyshit.Utilities.EXTRA_POEM_TITLE
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.popup_window.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
 
         adapter = PoemCoverAdapter(this, PoemDataService.poemList){poem ->
 
@@ -28,16 +35,28 @@ class MainActivity : AppCompatActivity() {
             poemSelectionIntent.putExtra(EXTRA_POEM_TITLE, poem)
 //            startActivity(poemSelectionIntent)
 
+
+
             val inflater: LayoutInflater = LayoutInflater.from(this)
 
 
-            val popupView = inflater.inflate(R.layout.popup_window, null)
+            val popupView = inflater.inflate(popup_window, null)
 
-            val width = LinearLayout.LayoutParams.WRAP_CONTENT
-            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+            val popupImage = popupView.findViewById<ImageView>(R.id.popupImage)
+            val popupText = popupView.findViewById<TextView>(R.id.popupText)
+
+            popupImage.setImageResource(this.resources.getIdentifier(poem.image, "drawable",this.packageName))
+            popupText.text = poem.title
+
+            val width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,300F,resources.displayMetrics).toInt()
+            val height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,600F,resources.displayMetrics).toInt()
+
+            Log.d("PIX", "width is $width and the height is $height")
             var focusable = true
 
             val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+            popupWindow.animationStyle = R.style.Animation_AppCompat_DropDownUp
 
             popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0)
 
